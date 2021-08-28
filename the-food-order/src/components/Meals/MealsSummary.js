7fadf2a9738ce67fa30366c218d4d117c38c5980
MD.Anis Molla <41534160+anichu@users.noreply.github.com>
2021-08-28 10:54:44 +0600

    Add files via upload

---
+++ the-food-order/src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food,Delivered To You</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, et.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum atque
+        deserunt nobis?
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
