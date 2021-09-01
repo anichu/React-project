d6c1b128f1629e32d3ba5ebeeeb47fc24fd117d9
MD.Anis Molla <41534160+anichu@users.noreply.github.com>
2021-09-01 11:04:44 +0600

    Add files via upload

---
+++ class-based-component-project/src/components/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error) {
+    console.log(error);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p>Something went to wrong!</p>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
